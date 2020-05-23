05d5198a0eac20f6cedb2776aee8ece054533cd7
Edward Mangini <emangini@mbp-emangini2.tracelinkcorp.com>
2020-05-23 19:22:46 -0400

    Fixed RedisToDoService

--- src/main/java/io/vertx/blueprint/todolist/service/RedisTodoService.java
+++ src/main/java/io/vertx/blueprint/todolist/service/RedisTodoService.java
@@ -9,12 +9,17 @@ import io.vertx.blueprint.todolist.entity.Todo;
 import io.vertx.core.json.Json;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.redis.client.Redis;
+import io.vertx.reactivex.redis.client.RedisAPI;
+import io.vertx.reactivex.redis.client.Response;
 import io.vertx.redis.client.RedisOptions;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * Redis implementation of {@link TodoService}.
@@ -23,38 +28,42 @@ import java.util.stream.Collectors;
  */
 public class RedisTodoService implements TodoService {
 
-  private final Vertx vertx;
-  private final RedisOptions config;
-  private final Redis redis;
+  private static  RedisAPI redis = null;
 
-  public RedisTodoService(Vertx vertx, RedisOptions config) {
+  private final Vertx vertx;
+  private final RedisOptions options;
+
+  public RedisTodoService(Vertx vertx, RedisOptions options) {
     this.vertx = vertx;
-    this.config = config;
-    this.redis = Redis.create(vertx, config);
+    this.options = options;
+    redis = RedisAPI.api(Redis.createClient(vertx, options));
   }
 
   @Override
   public Completable initData() {
     Todo sample = new Todo(Math.abs(ThreadLocalRandom.current().nextInt(0, Integer.MAX_VALUE)),
       "Something to do...", false, 1, "todo/ex");
-    return insert(sample).toCompletable();
+    return insert(sample).ignoreElement();
   }
 
   @Override
   public Single<Todo> insert(Todo todo) {
     final String encoded = Json.encodePrettily(todo);
-    return redis.rxHset(Constants.REDIS_TODO_KEY, String.valueOf(todo.getId()), encoded)
-      .map(e -> todo);
+    return redis
+            .rxHset(List.of(Constants.REDIS_TODO_KEY, String.valueOf(todo.getId()), encoded))
+            .map(response -> todo)
+            .toSingle();
   }
 
   @Override
   public Single<List<Todo>> getAll() {
     return redis.rxHvals(Constants.REDIS_TODO_KEY)
-      .map(e -> e.stream()
-        .map(x -> new Todo((String) x))
-        .collect(Collectors.toList())
-      );
-  }
+            .map(response -> StreamSupport.stream(response.spliterator(), false)
+                    .map(Object::toString)
+                    .map(Todo::new)
+                    .collect(Collectors.toList())
+      ).toSingle(Collections.singletonList(new Todo("")));
+}
 
   @Override
   public Maybe<Todo> getCertain(String todoID) {
@@ -62,26 +71,26 @@ public class RedisTodoService implements TodoService {
       return Maybe.empty();
     }
     return redis.rxHget(Constants.REDIS_TODO_KEY, todoID)
-      .toMaybe()
-      .map(Todo::new);
+      .map(response -> new Todo(response.toString()));
   }
 
   @Override
-  public Maybe<Todo> update(String todoId, Todo newTodo) {
-    return getCertain(todoId)
-      .map(old -> old.merge(newTodo))
-      .flatMap(e -> insert(e)
-        .flatMapMaybe(r -> Maybe.just(e))
+  public Maybe<Todo> update(String key, Todo todo) {
+    return getCertain(key)
+            .map(value -> value.merge(todo))
+            .flatMap(
+                    event -> insert(event)
+                            .flatMapMaybe(response -> Maybe.just(event))
       );
   }
 
   @Override
   public Completable delete(String todoId) {
-    return redis.rxHdel(Constants.REDIS_TODO_KEY, todoId).toCompletable();
+      return redis.rxHdel(List.of(Constants.REDIS_TODO_KEY, todoId)).ignoreElement();
   }
 
   @Override
   public Completable deleteAll() {
-    return redis.rxDel(Constants.REDIS_TODO_KEY).toCompletable();
+    return redis.rxDel(List.of(Constants.REDIS_TODO_KEY)).ignoreElement();
   }
 }
